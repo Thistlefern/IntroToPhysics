a7d467aff08128119f9934f0d881475b2033e4b9
Josie DeRosier <s209055@students.aie.edu>
2021-09-08 17:50:30 -0700

    Worked on ragdoll physics for assessment, need to implement raycast and triggers.

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    public GameObject seat;
+    public Rigidbody butt;
+    public GameObject drama;
+    Vector3 tempRotation;
+    Vector3 tempPos;
+    public bool isRagdolling;
+
+    private void Start()
+    {
+        isRagdolling = false;
+        tempRotation = Vector3.zero;
+        drama.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isRagdolling)
+        {
+            tempRotation.x = 115 * seat.transform.rotation.x;
+            tempRotation.x -= 90;
+            butt.rotation = Quaternion.Euler(tempRotation);
+
+            tempPos = seat.transform.position;
+            tempPos.y += 0.57f;
+            butt.position = tempPos;
+        }
+        else
+        {
+            drama.SetActive(true);
+        }
+    }
+}
